0acc9550b3e77cefae9c64521b73ab7705f41919
Shivam Singh <136737397+shivamsinghAIMLops32@users.noreply.github.com>
2026-09-23 13:07:22 +0530

    complex number class

--- src/com/oops/ComplexMathOperation.java
+++ src/com/oops/ComplexMathOperation.java
@@ -6,11 +6,15 @@ public class ComplexMathOperation {
         ComplexNumber z1 = new ComplexNumber(5, 2);
         z1.print();
 
-        ComplexNumber z2 = new ComplexNumber(10, 20);
+        ComplexNumber z2 = new ComplexNumber(10, -20);
         z2.print();
 
         System.out.println("after operation");
         z2.add(z1);
         z2.print();
+
+        System.out.println("after multiply operation");
+        z1.multiply(z2);
+        z1.print();
     }
 }
--- src/com/oops/ComplexNumber.java
+++ src/com/oops/ComplexNumber.java
@@ -1,30 +1,62 @@
 package com.oops;
 
 public class ComplexNumber {
-    int realPart;
-    int imaginaryPart;
+    double realPart;
+    double imaginaryPart;
     final static String iota = "i";
 
     // default constructor
     ComplexNumber(){}
 
-    ComplexNumber(int realPart,int imaginaryPart){
+    ComplexNumber(double realPart,double imaginaryPart){
         this.realPart = realPart; this.imaginaryPart= imaginaryPart;
     }
 
-    void print(){
-        System.out.println("real part is " + this.realPart+" imaginary part is "+this.imaginaryPart+this.iota );
+    void print() {
+        if (this.imaginaryPart >= 0) {
+            System.out.println(this.realPart + " + " + this.imaginaryPart + iota);
+        } else {
+            System.out.println(this.realPart + " - " + Math.abs(this.imaginaryPart) + iota);
+        }
     }
 
-    void add(ComplexNumber cp2){
-        this.realPart += cp2.realPart;
-        this.imaginaryPart += cp2.imaginaryPart;
+    void add(ComplexNumber z){
+        this.realPart += z.realPart;
+        this.imaginaryPart += z.imaginaryPart;
     }
 
-    void subtract(ComplexNumber cp2){
-        this.realPart -= cp2.realPart;
-        this.imaginaryPart -= cp2.imaginaryPart;
+    void subtract(ComplexNumber z){
+        this.realPart -= z.realPart;
+        this.imaginaryPart -= z.imaginaryPart;
     }
+
+    void multiply(ComplexNumber z){
+        // (x + yi)(a + bi) = (xa + xbi)+ (yai + ybi^2)  where i^2 is -1
+        //   xa + xbi + yai - yb
+        // so finally group imaginary and real together => (za-yb)+(xb + ya)i
+        double newReal = this.realPart * z.realPart - this.imaginaryPart * z.imaginaryPart;
+        double newImaginary = this.realPart * z.imaginaryPart + this.imaginaryPart * z.realPart;
+
+        this.realPart = newReal;
+        this.imaginaryPart = newImaginary;
+    }
+
+    void divide(ComplexNumber z) {
+        // Formula: (x + yi) / (a + bi) = [(xa + yb) + (ya - xb)i] / (a^2 + b^2)
+
+        double denominator = z.realPart * z.realPart + z.imaginaryPart * z.imaginaryPart;
+
+        if (denominator == 0) {
+            throw new ArithmeticException("Division by zero (complex number 0 + 0i)");
+        }
+
+        double newReal = (this.realPart * z.realPart + this.imaginaryPart * z.imaginaryPart) / denominator;
+        double newImaginary = (this.imaginaryPart * z.realPart - this.realPart * z.imaginaryPart) / denominator;
+
+        this.realPart = newReal;
+        this.imaginaryPart = newImaginary;
+    }
+
 }
 
 
